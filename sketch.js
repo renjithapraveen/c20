aee84fc827fde4b6230f1267532250980442e7b3
renjithapraveen <63199113+renjithapraveen@users.noreply.github.com>
2020-04-09 12:32:06 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,28 +1,31 @@
 
-var r = 0;
-var g = 50;
-var b=255;
+var fixedRect, movingRect;
 
-// INITIALIZE ALL YOUR VARIABLES
-// Ideally you would create 3 variables. red blue green or r, g, b
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(400, 100, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400, 800,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
 
-
-function setup(){
-
-  createCanvas(1200,400);
+  movingRect.velocityY = -5;
+  fixedRect.velocityY = +5;
 }
 
+function draw() {
+  background(0,0,0);  
 
-function draw(){
-
-  // change the value of Red based on the mouse movement about the X axis
-  // change the value of Green based on the mouse movement about the X axis
-  // change the value of Blue based on the mouse movement about the X axis
-
-  // Use the map() function to do so. 
-
-  // Pass the values to the background() function you have learnt previously.
-
-  // Create an ellipse that will move around with you mouse about the X axis.
-  // Remember to fill the canvas with white paint before creating the ellipse.
+  if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
+      && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2) {
+    movingRect.velocityX = movingRect.velocityX * (-1);
+    fixedRect.velocityX = fixedRect.velocityX * (-1);
+  }
+  if (movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2
+    && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2){
+    movingRect.velocityY = movingRect.velocityY * (-1);
+    fixedRect.velocityY = fixedRect.velocityY * (-1);
+  }
+  drawSprites();
 }
